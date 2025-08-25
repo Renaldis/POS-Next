98b6c3c50cf6e37b13c15523bfcd9eedd91f3f63
renaldis <renaldis142@gmail.com>
2025-08-25 22:30:56 +0700

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,14 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+// custom eslint config
+eslintConfig.push({
+  rules:{
+    'react-hooks/exhautive-deps':'off', // menonaktifkan eslint reactHooks seperti useEffect yang terlalu strict dngan dependencies nya,
+    "react/jsx-key":"off", // menonaktifkan kewajiban menggunakan key, tpi klo off klo build ada error harus benerin sendiri
+    "@typescript-eslint/no-explicit-any":"off", // sebenarnya kita gaboleh pake any, tpi kita akan pake midtrans yg belum support penuh dengan react
+    "@typescript-eslint/no-unused-vars":"off",
+  }
+})
+
 export default eslintConfig;
