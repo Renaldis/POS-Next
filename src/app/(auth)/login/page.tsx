132570f82458ffce54d3ceadf533a43130bdc00c
renaldis <renaldis142@gmail.com>
2025-08-28 14:32:46 +0700

    feat: add validation with react hook form n zod

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "./_components/login";
+
+export const metadata = {
+  title: "POST Next | Login",
+};
+
+export default function Loginpage() {
+  return <Login />;
+}
---
+++ src/validations/auth-validation.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email("Please enter a valid email"),
+  password: z.string().min(1, "Password is required"),
+});
+
+// export type LoginForm = {
+//   email: string;
+//   password: string;
+// };
+
+// cara praktis
+export type LoginForm = z.infer<typeof loginSchema>;
