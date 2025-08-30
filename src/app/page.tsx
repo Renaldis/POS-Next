c99ebe511857568268b902284fcbb015a06acb05
renaldis <renaldis142@gmail.com>
2025-08-30 21:55:46 +0700

    feat: add sidebar menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button>Hello</Button>
-      <DarkmodeToggle />
+    <div className="bg-muted flex flex-col justify-center items-center h-screen space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Renaldi Saputra</h1>
+      <Link href={"/admin"}>
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
