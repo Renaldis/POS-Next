dc52d359a0d9e45166f4fe1bcd842f80c6506005
renaldis <renaldis142@gmail.com>
2025-09-03 16:16:24 +0700

    feat: create user

---
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -0,0 +1,115 @@
+import FormInput from "@/components/common/form-input";
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+import {
+  INIITAL_CREATE_USER_FORM,
+  INITIAL_STATE_CREATE_USER,
+} from "@/constant/auth-constant";
+import {
+  CreateUserForm,
+  createUserSchema,
+} from "@/validations/auth-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { startTransition, useActionState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { createUser } from "../actions";
+import { toast } from "sonner";
+
+export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
+  const form = useForm<CreateUserForm>({
+    resolver: zodResolver(createUserSchema),
+    defaultValues: INIITAL_CREATE_USER_FORM,
+  });
+
+  const [createUserState, createUserAction, isPendingCreateUser] =
+    useActionState(createUser, INITIAL_STATE_CREATE_USER);
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const formData = new FormData();
+    Object.entries(data).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    // start transition -> function dari React untuk menandai bahwa update ini bukan prioritas utama,
+    // sehingga UI tetap responsif (tidak nge-freeze). Biasanya dipakai untuk action async atau navigasi.
+    startTransition(() => {
+      createUserAction(formData);
+    });
+    // loginAction(formData) → memanggil server action login dengan data form. Nanti hasilnya otomatis update ke loginState dan isPendingLogin.
+  });
+
+  useEffect(() => {
+    if (createUserState?.status === "error") {
+      toast.error("Login failed", {
+        description: createUserState?.errors?._form?.[0],
+      });
+    }
+
+    if (createUserState?.status === "success") {
+      toast.success("Create User Success");
+      form.reset();
+      document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
+      refetch();
+    }
+  }, [createUserState]);
+
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <Form {...form}>
+        <DialogHeader>
+          <DialogTitle>Create User</DialogTitle>
+          <DialogDescription>register a new user</DialogDescription>
+        </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <FormInput
+            form={form}
+            type="text"
+            name="name"
+            label="Name"
+            placeholder="Insert your name"
+          />
+          <FormInput
+            form={form}
+            type="email"
+            name="email"
+            label="Email"
+            placeholder="Insert email here"
+          />
+          <FormInput
+            form={form}
+            name="role"
+            label="Role"
+            placeholder="Insert your role"
+          />
+          <FormInput
+            form={form}
+            type="password"
+            name="password"
+            label="Password"
+            placeholder="******"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant={"outline"}>Cancel</Button>
+            </DialogClose>
+            <Button type="submit">
+              {isPendingCreateUser ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                "Create"
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </DialogContent>
+  );
+}
