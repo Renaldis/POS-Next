da6c1deeef22c2bbbdf2aa7238ef5a307d6693bf
renaldis <renaldis142@gmail.com>
2025-09-17 18:49:33 +0700

    feat: create order

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils";
 import { Table } from "@/validations/table-validation";
 import { HEADER_TABLE_TABLE } from "@/constant/table-constant";
 import { HEADER_TABLE_ORDER } from "@/constant/order-constant";
+import DialogCreateOrder from "./dialog-create-order";
 // import DialogCreateTable from "./dialog-create-table";
 // import DialogUpdateTable from "./dialog-update-table";
 // import DialogDeleteTable from "./dialog-delete-table";
@@ -61,6 +62,19 @@ export default function OrderManagement() {
         });
 
       return result;
+    },
+  });
+
+  const { data: tables, refetch: refetchTables } = useQuery({
+    queryKey: ["tables"],
+    queryFn: async () => {
+      const result = await supabase
+        .from("tables")
+        .select("*")
+        .order("created_at")
+        .order("status");
+
+      return result.data;
     },
   });
 
@@ -147,7 +161,7 @@ export default function OrderManagement() {
             <DialogTrigger asChild>
               <Button variant="outline">Create</Button>
             </DialogTrigger>
-            {/* <DialogCreateTable refetch={refetch} /> */}
+            <DialogCreateOrder tables={tables} refetch={refetch} />
           </Dialog>
         </div>
       </div>
---
+++ src/app/(dashboard)/order/actions.ts
@@ -0,0 +1,144 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { OrderFormState } from "@/types/order";
+
+import { TableFormState } from "@/types/table";
+import { orderFormSchema } from "@/validations/order-validation";
+import { tableSchema } from "@/validations/table-validation";
+
+export async function createOrder(
+  prevState: OrderFormState,
+  formData: FormData
+) {
+  const validatedFields = orderFormSchema.safeParse({
+    customer_name: formData.get("customer_name"),
+    table_id: formData.get("table_id"),
+    status: formData.get("status"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      status: "error",
+      errors: {
+        ...validatedFields.error.flatten().fieldErrors,
+        _form: [],
+      },
+    };
+  }
+
+  const supabase = await createClient();
+
+  const orderId = `RendsCafe-${Date.now()}`;
+
+  const [orderResult, tableResult] = await Promise.all([
+    supabase.from("orders").insert({
+      order_id: orderId,
+      customer_name: validatedFields.data.customer_name,
+      table_id: validatedFields.data.table_id,
+      status: validatedFields.data.status,
+    }),
+
+    supabase
+      .from("tables")
+      .update({
+        status:
+          validatedFields.data.status === "reserved"
+            ? "reserved"
+            : "unavailable",
+      })
+      .eq("id", validatedFields.data.table_id),
+  ]);
+
+  const orderError = orderResult.error;
+  const tableError = tableResult.error;
+
+  if (orderError || tableError) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [
+          ...(orderError ? [orderError.message] : []),
+          ...(tableError ? [tableError.message] : []),
+        ],
+      },
+    };
+  }
+
+  return {
+    status: "success",
+  };
+}
+
+export async function updateTable(
+  prevState: TableFormState,
+  formData: FormData
+) {
+  const validatedFields = tableSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+    capacity: parseInt(formData.get("capacity") as string),
+    status: formData.get("status"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      status: "error",
+      errors: {
+        ...validatedFields.error.flatten().fieldErrors,
+        _form: [],
+      },
+    };
+  }
+
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("tables")
+    .update({
+      name: validatedFields.data.name,
+      description: validatedFields.data.description,
+      capacity: validatedFields.data.capacity,
+      status: validatedFields.data.status,
+    })
+    .eq("id", formData.get("id"));
+
+  if (error) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [error.message],
+      },
+    };
+  }
+
+  return {
+    status: "success",
+  };
+}
+
+export async function deleteTable(
+  prevState: TableFormState,
+  formData: FormData
+) {
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("tables")
+    .delete()
+    .eq("id", formData.get("id"));
+
+  if (error) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [error.message],
+      },
+    };
+  }
+
+  return { status: "success" };
+}
