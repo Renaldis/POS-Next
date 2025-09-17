5ef533d12bb92d449a9f9dee34d5e44776e2aee6
renaldis <renaldis142@gmail.com>
2025-09-17 16:10:53 +0700

    feat: order list

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,8 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "WPU Cafe | Table Management",
+};
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
